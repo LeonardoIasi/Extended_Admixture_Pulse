d447e91c2064845af9e52cc7955d841da9933d7b
leonardo_iasi <leonardo_iasi@eva.mpg.de>
2019-12-18 13:37:06 +0100

    not really changed

--- run_snakemake_eva.sh
+++ run_snakemake_eva.sh
@@ -1,5 +1,5 @@
-mkdir ../autosnake_Recent_Sampling
+mkdir ../autosnake_Variyng_Time_of_Recent_sampling_GF_l_fixed_Recomn_Map_Hap_Map
 snakemake -w 120 --jobname "_{rule}_{jobid}" -j500 \
     --cluster-config config/cluster.yaml \
-    --cluster "qsub -q all.q -l h_vmem={cluster.mem} -o ../autosnake_Recent_Sampling/ -e ../autosnake_Recent_Sampling/ -cwd -V -S /bin/bash" $*  
+    --cluster "qsub -q all.q -l h_vmem={cluster.mem} -o ../autosnake_Variyng_Time_of_Recent_sampling_GF_l_fixed_Recomn_Map_Hap_Map  -e ../autosnake_Variyng_Time_of_Recent_sampling_GF_l_fixed_Recomn_Map_Hap_Map -cwd -V -S /bin/bash" $*  
 
