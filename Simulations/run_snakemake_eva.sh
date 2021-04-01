4221606050ca5dc1717b208b8b80fd160799bc16
leonardo_iasi <leonardo_iasi@eva.mpg.de>
2021-04-01 14:44:08 +0200

    new scripts and figures for the revision

---
+++ Simulations/run_snakemake_eva.sh
@@ -0,0 +1,5 @@
+mkdir ../../autosnake_Fig_2_C_corrected_revision
+snakemake -s Snakemake_Simulation_pipeline_efficient.py --keep-going -w 120 --jobname "_{rule}_{jobid}" -j800 \
+    --cluster-config config/cluster.yaml \
+    --cluster "qsub -q all.q -l h_vmem={cluster.mem} -o ../../autosnake_Fig_2_C_corrected_revision  -e ../../autosnake_Fig_2_C_corrected_revision -cwd -V -S /bin/bash" $*  
+
